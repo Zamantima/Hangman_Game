b4a9e6b6c51b0335e746e1023f85a123d4bd90b3
Zamantima Thela <>
2022-08-10 08:26:35 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,46 @@
+from replit import clear
+import random
+
+lives = 6
+
+from hangman_art import logo, stages 
+print(logo)
+
+from hangman_words import word_list
+
+chosen_word = random.choice(word_list) 
+
+display = []
+
+for _ in range(len(chosen_word)):
+  display += "_"
+print(display)
+
+end_of_game = False
+
+while not end_of_game:
+  guess = input("Guess a letter ").lower()
+
+  clear()
+  
+  if guess in display:
+    print(f"You have alreadt guessed {guess}")
+  
+  for position in range(len(chosen_word)):
+    if guess == chosen_word[position]:
+      display[position] = guess
+
+  if guess not in chosen_word:
+    lives -= 1
+    print(f"You guessed {guess}, that is not in the word. You lose a life.")
+    if lives == 0:
+      end_of_game = True
+      print("You lose. Game Over!")
+      
+  print(display)
+
+  if "_" not in display:
+    end_of_game = True
+    print("Game Over. You win!")
+
+  print(stages[lives])
